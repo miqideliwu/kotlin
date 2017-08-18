a26c765189cc9389fde993bb2b7b6898716c9a68
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2017-08-18 15:34:56 +0300

    Temporarily ignore some multiplatform IDE tests

--- idea/tests/org/jetbrains/kotlin/idea/caches/resolve/MultiModuleHighlightingTest.kt
+++ idea/tests/org/jetbrains/kotlin/idea/caches/resolve/MultiModuleHighlightingTest.kt
@@ -45,6 +45,7 @@ import org.jetbrains.kotlin.idea.util.application.executeWriteCommand
 import org.jetbrains.kotlin.idea.util.application.runWriteAction
 import org.jetbrains.kotlin.idea.util.projectStructure.sdk
 import org.jetbrains.kotlin.test.TestJdkKind.FULL_JDK
+import org.junit.Ignore
 
 open class MultiModuleHighlightingTest : AbstractMultiModuleHighlightingTest() {
     override fun getTestDataPath() = PluginTestCaseBase.getTestDataPathBase() + "/multiModuleHighlighting/"
@@ -207,6 +208,8 @@ open class MultiModuleHighlightingTest : AbstractMultiModuleHighlightingTest() {
         }
     }
 
+    // Some tests are ignored below. They fail because <error> markers are not stripped correctly in multi-module highlighting tests.
+    // TODO: fix this in the framework and unignore the tests
     class MultiPlatform : AbstractMultiModuleHighlightingTest() {
         override fun getTestDataPath() = "${PluginTestCaseBase.getTestDataPathBase()}/multiModuleHighlighting/multiplatform/"
 
@@ -222,7 +225,7 @@ open class MultiModuleHighlightingTest : AbstractMultiModuleHighlightingTest() {
             doMultiPlatformTest(TargetPlatformKind.Jvm[JvmTarget.JVM_1_6], TargetPlatformKind.JavaScript)
         }
 
-        fun testHeaderPartiallyImplemented() {
+        fun ignore_testHeaderPartiallyImplemented() {
             doMultiPlatformTest(TargetPlatformKind.Jvm[JvmTarget.JVM_1_6])
         }
 
@@ -264,7 +267,7 @@ open class MultiModuleHighlightingTest : AbstractMultiModuleHighlightingTest() {
             })
         }
 
-        fun testNestedClassWithoutImpl() {
+        fun ignore_testNestedClassWithoutImpl() {
             doMultiPlatformTest(TargetPlatformKind.Jvm[JvmTarget.JVM_1_6])
         }
     }
